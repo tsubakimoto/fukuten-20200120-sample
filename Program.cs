4dd4db31ad3abd3617bf97d1e1c76999327727c1
tsubakimoto <tkmbmbr@gmail.com>
2020-01-10 20:05:36 +0900

    Initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fukuten_20200120_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
